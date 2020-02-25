c7c57fa3908101227348d0a2187b5e291db2ef82
Vivica Ma <vivicacongma@gmail.com>
2020-02-25 03:17:00 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -46,5 +46,22 @@ def nyc_pigeon_organizer(original_data)
      
     return_data
 end 
-    
-    
+
+pigeon_data = {
+  :color => {
+    :brown => ["Luca"],
+    :black => ["Lola"],
+  },
+  :gender => {
+    :male => ["Luca"],
+    :female => ["Lola"]
+  },
+  :lives => {
+    "Central Park" => ["Lola"],
+    "Library" => ["Luca"]
+  }
+}
+
+nyc_pigeon_organizer(pigeon_data)
+
+
